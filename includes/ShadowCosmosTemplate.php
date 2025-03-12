08b3f67dc477de5ab2adb4b0be390a6374c80e54
catrone3 <catrone3@gmail.com>
2025-03-12 05:20:34 +0000

    update to be self contained

--- includes/ShadowCosmosTemplate.php
+++ includes/ShadowCosmosTemplate.php
@@ -22,7 +22,7 @@ class ShadowCosmosTemplate extends CosmosTemplate {
 		global $wgOut;
 		
 		$kofiHtml = '<div class="shadowcosmos-kofi-support">' .
-			'<a href="https://ko-fi.com/YOUR_KOFI_USERNAME" target="_blank">' .
+			'<a href="https://ko-fi.com/catrone3" target="_blank">' .
 			'<img src="https://storage.ko-fi.com/cdn/kofi_button_blue.png" ' .
 			'alt="Support Me on Ko-fi" class="kofi-button" />' .
 			'<span>Support this Wiki on Ko-fi</span>' .
--- includes/SkinShadowCosmos.php
+++ includes/SkinShadowCosmos.php
@@ -1,33 +1,62 @@
 <?php
 
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Skins\SkinMustache;
 
 /**
- * SkinTemplate class for ShadowCosmos skin
+ * SkinMustache class for ShadowCosmos skin
  * @ingroup Skins
  */
-class SkinShadowCosmos extends SkinCosmos {
+class SkinShadowCosmos extends SkinMustache {
 	/** @var string */
 	public $skinname = 'shadowcosmos';
 	/** @var string */
 	public $stylename = 'ShadowCosmos';
-	/** @var string */
-	public $template = 'ShadowCosmosTemplate';
 
 	/**
-	 * @param OutputPage $out
+	 * @inheritDoc
 	 */
-	public function initPage( OutputPage $out ) {
-		parent::initPage( $out );
-		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1.0' );
+	public function __construct( $options = [] ) {
+		$options['templateDirectory'] = __DIR__ . '/../templates';
+		parent::__construct( $options );
 	}
 
 	/**
-	 * @return array
+	 * @inheritDoc
+	 */
+	public function getTemplateData(): array {
+		$data = parent::getTemplateData();
+		
+		// Add custom data for our templates
+		$data['msg-shadowcosmos-kofi-support'] = $this->msg( 'shadowcosmos-kofi-support' )->text();
+		$data['html-kofi-button'] = $this->getKofiButton();
+		
+		// Add site name and tagline
+		$data['site-name'] = $this->msg( 'sitetitle' )->text();
+		$data['site-tagline'] = $this->msg( 'sitesubtitle' )->text();
+		
+		return $data;
+	}
+	
+	/**
+	 * @inheritDoc
 	 */
 	public function getDefaultModules() {
 		$modules = parent::getDefaultModules();
+		$modules['styles'][] = 'skins.shadowcosmos';
+		$modules['scripts'][] = 'skins.shadowcosmos.js';
 		return $modules;
 	}
+	
+	/**
+	 * Get the Ko-fi support button HTML
+	 * @return string HTML
+	 */
+	private function getKofiButton() {
+		return '<a href="https://ko-fi.com/catrone3" target="_blank" class="kofi-button">' .
+			'<img src="https://storage.ko-fi.com/cdn/kofi_button_blue.png" ' .
+			'alt="Support Me on Ko-fi" height="36" />' .
+			'<span>Support this Wiki on Ko-fi</span>' .
+			'</a>';
+	}
 }
-
